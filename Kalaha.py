0713047a4866f7dbaa442220cdf9bebeb4d47dc8
Lucas-He <57799711+Lucas-He@users.noreply.github.com>
2021-12-04 18:12:02 +0100

    Add files via upload

---
+++ Kalaha.py
@@ -0,0 +1,230 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+
+
+"""
+# Field
+field_len = 6
+start_filling = 3
+field = np.zeros((2, field_len)).astype(int) + start_filling
+# Goals
+goals = np.zeros(2).astype(int)
+# Player
+player = 0
+"""
+def start_minimax(field, goals, player, sp=1, md=3):
+    moves = find_moves(field, player)
+    moves = quick_eval(field, moves, player)
+    move_vals = np.zeros(len(moves))
+    ab=[-np.inf, np.inf]
+    for cm, m in enumerate(moves):
+        undo_inf = apply_move(field, goals, player, m)
+        move_vals[cm] = min_s(field, goals, (player+1)%2, d=1, ab=ab[:2], sp=sp, md=md)
+        undo_move(field, goals, player, undo_inf)
+        if move_vals[cm] > ab[0]:
+            ab[0] = move_vals[cm]
+        if move_vals[cm] > ab[1]:
+            return move_vals[cm]
+    max_val = np.max(move_vals)
+    max_moves = np.where(move_vals == max_val)[0]
+    rand_max_move = np.random.choice(max_moves, 1)[0]
+    return moves[rand_max_move], max_val
+    
+
+def min_s(field, goals, player, d, ab, sp, md):
+    if d==md:
+        return sp*evaluate(goals)
+    if np.sum(field[player]) == 0:
+        if goals[player] - goals[(player+1)%2] > 0:
+            if d==1:
+                return -9999
+            return -999
+        elif goals[player] - goals[(player+1)%2] < 0:
+            if d==1:
+                return 9999
+            return 999
+        else:
+            return 0
+    moves = find_moves(field, player)
+    moves = quick_eval(field, moves, player)
+    move_vals = np.zeros(len(moves))
+    for cm, m in enumerate(moves):
+        undo_inf = apply_move(field, goals, player, m)
+        move_vals[cm] = max_s(field, goals, (player+1)%2, d=d+1, ab=ab[:2], sp=sp, md=md)
+        undo_move(field, goals, player, undo_inf)
+        if move_vals[cm] < ab[1]:
+            ab[1] = move_vals[cm]
+        if move_vals[cm] < ab[0]:
+            return move_vals[cm]
+    return np.min(move_vals)
+
+
+def max_s(field, goals, player, d, ab, sp, md):
+    if d==md:
+        return sp*evaluate(goals)
+    if np.sum(field[player]) == 0:
+        if goals[player] - goals[(player+1)%2] > 0:
+            if d==1:
+                return 9999
+            return 999
+        elif goals[player] - goals[(player+1)%2] < 0:
+            if d==1:
+                return -9999
+            return -999
+        else:
+            return 0
+    moves = find_moves(field, player)
+    moves = quick_eval(field, moves, player)
+    move_vals = np.zeros(len(moves))
+    for cm, m in enumerate(moves):
+        undo_inf = apply_move(field, goals, player, m)
+        move_vals[cm] = min_s(field, goals, (player+1)%2, d=d+1, ab=ab[:2], sp=sp, md=md)
+        undo_move(field, goals, player, undo_inf)
+        if move_vals[cm] > ab[0]:
+            ab[0] = move_vals[cm]
+        if move_vals[cm] > ab[1]:
+            return move_vals[cm]
+    return np.max(move_vals)
+
+
+def evaluate(goals):
+    return goals[0] - goals[1]
+    
+
+def apply_move(field, goals, player, move):
+    to_distrib = field[player, move]
+    n = len(field[0])
+    to_all = int(to_distrib // (2*n + 1))
+    rest = int(to_distrib % (2*n + 1))
+    field[player, move] = 0
+    field += to_all
+    goals[player] += to_all
+    undo_info = np.zeros(5).astype(int) - 1
+    undo_info[:2] = np.array([move, to_distrib])
+    row_to_check = -1
+    fin_p = -1
+    if player == 0:
+        if rest <= move:
+            field[0][move-rest:move] += 1
+            row_to_check = move - rest
+            fin_p = 0
+        elif rest == move + 1:
+            field[0][:move] += 1
+            goals[0] += 1
+            row_to_check = -1
+            fin_p = -1
+        elif rest <= move + 1 + n:
+            field[0][:move] += 1
+            goals[0] += 1
+            field[1][:rest-move-1] += 1
+            row_to_check = rest - move - 2
+            fin_p = 1
+        else:
+            field[0][:move] += 1
+            goals[0] += 1
+            field[1] += 1
+            field[0][move + 1 + n - rest:] += 1
+            row_to_check = move + 1 + 2*n - rest
+            fin_p = 0
+        if row_to_check != -1 and fin_p == 0 and field[0][row_to_check] == 1:
+            if field[1][row_to_check] > 1:
+                goals[0] += field[0][row_to_check] + field[1][row_to_check]
+                undo_info[3] = field[0][row_to_check]
+                undo_info[4] = field[1][row_to_check]
+                field[:, row_to_check] = 0
+                undo_info[2] = row_to_check
+            else:
+                undo_info[2] = -1
+        else:
+            undo_info[2] = -1
+    else:
+        if rest <= n - move - 1:
+            field[1][move+1:move+rest+1] += 1
+            row_to_check = move + rest
+            fin_p = 1
+        elif rest == n - move:
+            field[1][move+1:] += 1
+            goals[1] += 1
+            row_to_check = -1
+            fin_p = -1
+        elif rest <= 2 * n - move:
+            field[1][move+1:] += 1
+            goals[1] += 1
+            field[0][n - move - rest:] += 1
+            row_to_check = 2*n - move - rest
+            fin_p = 0
+        else:
+            field[1][move+1:] += 1
+            goals[1] += 1
+            field[0] += 1
+            field[1][:rest-n-(n-move)] += 1
+            row_to_check = rest-n-(n-move)-1
+            fin_p = 1
+        if row_to_check != -1 and fin_p == 1 and field[1][row_to_check] == 1:
+            if field[0][row_to_check] > 1:
+                goals[1] += field[0][row_to_check] + field[1][row_to_check]
+                undo_info[3] = field[1][row_to_check]
+                undo_info[4] = field[0][row_to_check]
+                field[:, row_to_check] = 0
+                undo_info[2] = row_to_check
+            else:
+                undo_info[2] = -1
+        else:
+            undo_info[2] = -1
+    return undo_info
+        
+
+def undo_move(field, goals, player, undo_info):
+    # Undo_info: [move, to_distrib, row_to_check, own_m, op_m]
+    n = len(field[0])
+    from_all = undo_info[1] // (2*n + 1)
+    rest = undo_info[1] % (2*n + 1)
+    if undo_info[2] != -1:
+        field[player][undo_info[2]] += undo_info[3]
+        field[(player+1)%2][undo_info[2]] += undo_info[4]
+        goals[player] -= (undo_info[3] + undo_info[4])
+    field -= from_all
+    m = undo_info[0]
+    goals[player] -= from_all
+    if player == 0:
+        if rest <= m:
+            field[0][m-rest:m] -= 1
+        elif rest == m + 1:
+            field[0][:m] -= 1
+            goals[0] -= 1
+        elif rest <= m + 1 + n:
+            field[0][:m] -= 1
+            goals[0] -= 1
+            field[1][:rest-m-1] -= 1
+        else:
+            field[0][:m] -= 1
+            goals[0] -= 1
+            field[1] -= 1
+            field[0][m + 1 + n - rest:] -= 1
+    else:
+        if rest <= n - m - 1:
+            field[1][m+1:m+rest+1] -= 1
+        elif rest == n - m:
+            field[1][m+1:] -= 1
+            goals[1] -= 1
+        elif rest <= 2 * n - m:
+            field[1][m+1:] -= 1
+            goals[1] -= 1
+            field[0][n - m - rest:] -= 1
+        else:
+            field[1][m+1:] -= 1
+            goals[1] -= 1
+            field[0] -= 1
+            field[1][:rest-n-(n-m)] -= 1
+    field[player, m] = undo_info[1]
+    return
+
+
+def quick_eval(field, moves, player):
+    return moves[np.argsort(field[player, moves])[::-1]]
+    
+    
+def find_moves(field, player):
+    moves = np.where(field[player] != 0)[0]
+    return np.array(moves)
---
+++ Kalaha_GUI.py
@@ -0,0 +1,172 @@
+# -*- coding: utf-8 -*-
+import tkinter as tk
+import numpy as np
+import Kalaha
+import gc
+
+
+class Kalaha_GUI(tk.Frame):
+    def __init__(self, parent):
+        tk.Frame.__init__(self, parent, width=1200, height=200)
+        # Field Length Text
+        self.Label_FL = tk.Label(self, text="Field length N:", anchor="w")
+        self.Label_FL.pack(side="left")
+        self.Label_FL.place(y=5,x=0,height=30, width=100)
+        # Field Length Entry
+        self.entry_FL = tk.Entry(self)
+        self.entry_FL.pack(side="left", padx=20)
+        self.entry_FL.place(y=5,x=100,height=30, width=50)
+        # Filling Text
+        self.Label_Fi = tk.Label(self, text="Filling M:", anchor="w")
+        self.Label_Fi.pack(side="left")
+        self.Label_Fi.place(y=5,x=160, height=30, width=100)
+        # Filling Entry
+        self.entry_Fi = tk.Entry(self)
+        self.entry_Fi.pack(side="left", padx=20)
+        self.entry_Fi.place(y=5,x=260, height=30, width=50)
+        # Depth Text
+        self.Label_depth = tk.Label(self, text="Search depth d:", anchor="w")
+        self.Label_depth.pack(side="left")
+        self.Label_depth.place(y=5,x=320, height=30, width=100)
+        # Depth Entry
+        self.entry_depth = tk.Entry(self)
+        self.entry_depth.pack(side="left", padx=20)
+        self.entry_depth.place(y=5,x=420, height=30, width=50)
+        # Start Button
+        self.button_Start = tk.Button(self, text="Start", command = self.start)
+        self.button_Start.pack(side="right")
+        self.button_Start.place(y=5,x=480,height=30, width=100)
+        # Auto Move Button
+        self.button_Auto = tk.Button(self, text="Auto Move", command = self.auto)
+        self.button_Auto.pack(side="right")
+        self.button_Auto.place(y=5,x=580,height=30, width=100)
+        # Fail Text
+        self.Label_fail = tk.Label(self, text="", anchor="w")
+        self.Label_fail.pack(side="left")
+        self.Label_fail.place(y=5,x=700, height=30, width=100)
+        # Field Length Info Text
+        self.Label_FLI = tk.Label(self, text="2-20", anchor="w")
+        self.Label_FLI.pack(side="left")
+        self.Label_FLI.place(y=35,x=100, height=30, width=50)
+        # Filling Info Text
+        self.Label_FiI = tk.Label(self, text="1-10", anchor="w")
+        self.Label_FiI.pack(side="left")
+        self.Label_FiI.place(y=35,x=260, height=30, width=50)
+        # Depth Info Text
+        self.Label_DI = tk.Label(self, text="2-10", anchor="w")
+        self.Label_DI.pack(side="left")
+        self.Label_DI.place(y=35,x=420, height=30, width=50)
+        #---------------------------------#
+        self.game_over = 0
+        self.player = 1
+        self.dest = 0
+        self.last_move = 0
+        self.n = 4
+        self.m = 3
+        self.depth = 2
+        self.field = np.zeros((2, self.n)).astype(int) + self.m
+        self.goals = np.zeros(2).astype(int)
+        
+        
+    def start(self):
+        self.destroy_old_field()
+        try:
+            self.game_over = 0
+            self.player = 1
+            self.dest = 0
+            self.last_move = 0
+            self.n = int(self.entry_FL.get())
+            self.m = int(self.entry_Fi.get())
+            self.depth = int(self.entry_depth.get())
+            if (self.n < 2 or self.n > 20 or self.m < 1 or self.m > 10 or self.depth < 2 or self.depth > 10):
+                self.fail()
+            else:
+                self.Label_fail.configure(text="")
+                self.field = np.zeros((2, self.n)).astype(int) + self.m
+                self.goals = np.zeros(2).astype(int)
+                self.create_visual_field()
+                self.dest = 1
+        except ValueError:
+            self.dest = 0
+            self.fail()
+        
+    def destroy_old_field(self):
+        if self.dest==1:
+            for u in self.field_buttons:
+                u.destroy()
+            for u in self.goal_labels:
+                u.destroy()
+ 
+    def create_visual_field(self):
+        self.field_buttons = []
+        self.button_count = 0
+        for i in range(2):
+            for j in range(self.n):
+                self.field_buttons.append(tk.Button(self, command=lambda k=self.n * i + j: self.change_field(pos=k)))
+                self.field_buttons[self.button_count].pack(side="left", padx=20)
+                self.field_buttons[self.button_count].place(y=80 + i * 50, x=100 + j * 50, height=50, width=50)
+                self.field_buttons[self.button_count].configure(bg="white", text="%d"%self.m)
+                self.button_count += 1
+        self.goal_labels = []
+        self.goal_labels.append(tk.Label(self, text="0", anchor="w"))
+        self.goal_labels.append(tk.Label(self, text="0", anchor="w"))
+        self.goal_labels[0].pack(side="left")
+        self.goal_labels[0].place(y=80,x=50,height=100, width=50)
+        self.goal_labels[0].configure(bg="white")
+        self.goal_labels[1].pack(side="left")
+        self.goal_labels[1].place(y=80,x=100 + self.n*50 ,height=100, width=50)
+        self.goal_labels[1].configure(bg="white")
+
+    def change_field(self, pos=0):
+        if not self.game_over:
+            if self.player == 0:
+                if pos < self.n and self.field[self.player, pos] != 0:
+                    _ = Kalaha.apply_move(self.field, self.goals, self.player, pos)
+                    self.player = (self.player + 1) % 2
+                    self.update_visual_field(pos)
+                    self.last_move = pos
+            else:
+                if pos >= self.n and self.field[self.player, pos%self.n] != 0:
+                    _ = Kalaha.apply_move(self.field, self.goals, self.player, pos%self.n)
+                    self.player = (self.player + 1) % 2
+                    self.update_visual_field(pos)
+                    self.last_move = pos
+                
+    def auto(self):
+        if not self.game_over:
+            if self.player == 0:
+                cm, cv = Kalaha.start_minimax(self.field, self.goals, self.player, sp=1, md=self.depth)
+                _ = Kalaha.apply_move(self.field, self.goals, self.player, cm)
+                self.player = (self.player + 1) % 2
+                self.update_visual_field(cm)
+                self.last_move = cm
+            else:
+                cm, cv = Kalaha.start_minimax(self.field, self.goals, self.player, sp=-1, md=self.depth)
+                _ = Kalaha.apply_move(self.field, self.goals, self.player, cm)
+                self.player = (self.player + 1) % 2
+                self.update_visual_field(self.n + cm)
+                self.last_move = self.n + cm
+    
+    def update_visual_field(self, green_space):
+        self.field_buttons[self.last_move].configure(bg="white")
+        for i in range(2):
+            for j in range(self.n):
+                self.field_buttons[i*self.n + j].configure(text="%d"%self.field[i, j])
+        self.goal_labels[0].configure(text="%d"%self.goals[0])
+        self.goal_labels[1].configure(text="%d"%self.goals[1])
+        self.field_buttons[green_space].configure(bg="green")
+    
+    def fail(self):
+        text_fail = "Check Inputs!"
+        self.Label_fail.configure(text=text_fail)
+            
+if __name__ == "__main__":
+    root = tk.Tk()
+    app=Kalaha_GUI(parent=root)
+    app.pack(fill="both", expand=True)
+    #Example(root).pack(fill="both", expand=True)
+    root.mainloop()
+    app.close()
+    layout = None
+    window = None
+    gc.collect()
